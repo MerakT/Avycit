0ff323683b9e90cc11c7a789bd5b3edf2c078346
Just-a-Spider <andre_arg_0116@outlook.com>
2024-02-22 08:44:50 -0500

    Added Token on LocalStorage

--- client/src/API/session.ts
+++ client/src/API/session.ts
@@ -1,3 +1,9 @@
 export async function getToken() {
-    // TODO
+    // check if there is a token in local storage
+    const token = localStorage.getItem('token');
+    if (token) {
+        return token;
+    }
+    // if there isn't, return null
+    return null;
 }
