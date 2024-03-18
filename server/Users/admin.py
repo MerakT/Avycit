db392d01bdd4e87db000a29d0e255b77106f86a0
Just-a-Spider <andre_arg_0116@outlook.com>
2024-03-18 15:37:20 -0500

    Added models Facultad and ProgAcad to admin.py

--- server/Users/admin.py
+++ server/Users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Usuario
+from .models import Usuario, Facultad, ProgAcad
 
 # Register your models here.
 admin.site.register(Usuario)
+admin.site.register(Facultad)
+admin.site.register(ProgAcad)
