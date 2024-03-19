bf9ff5f9e031ebc045ae3323634d5cdfd43250ee
Just-a-Spider <andre_arg_0116@outlook.com>
2024-03-19 15:46:30 -0500

    Update URLs for raw and clean problems

--- server/Problems/urls.py
+++ server/Problems/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('rawproblems/', RawProblemList.as_view(), name='raw-problem-list'),
-    path('rawproblems/<int:pk>/', RawProblemDetail.as_view(), name='raw-problem-detail'),
-    path('cleanproblems/', CleanProblemList.as_view(), name='clean-problem-list'),
-    path('cleanproblems/<int:pk>/', CleanProblemDetail.as_view(), name='clean-problem-detail'),
+    path('raw/', RawProblemList.as_view(), name='raw-problem-list'),
+    path('raw/<int:pk>/', RawProblemDetail.as_view(), name='raw-problem-detail'),
+    path('clean/', CleanProblemList.as_view(), name='clean-problem-list'),
+    path('clean/<int:pk>/', CleanProblemDetail.as_view(), name='clean-problem-detail'),
 ]
