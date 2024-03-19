a8298c840f71ddab5d87a99b763684b5905deecf
Just-a-Spider <andre_arg_0116@outlook.com>
2024-03-19 15:48:09 -0500

    Refactor get_clean_data method in RawProblemSerializer

--- server/Problems/serializers.py
+++ server/Problems/serializers.py
@@ -36,8 +36,10 @@ class RawProblemSerializer(serializers.ModelSerializer):
 
     def get_clean_data(self, obj):
         try:
-            clean_problem = CleanProblem.objects.get(raw_problem=obj.id)
-            return SimpleCleanProblemSerializer(clean_problem).data
+            clean_problems = CleanProblem.objects.filter(raw_problem=obj.id)
+            if not clean_problems:
+                return None
+            return SimpleCleanProblemSerializer(clean_problems, many=True).data
         except CleanProblem.DoesNotExist:
             return None
 
