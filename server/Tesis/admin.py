e74acf95a10ddce3693b8d46e7666f1bacaf2d8b
Just-a-Spider <andre_arg_0116@outlook.com>
2024-04-08 19:59:56 -0500

    Refactor admin.py to remove unnecessary registrations

--- server/Tesis/admin.py
+++ server/Tesis/admin.py
@@ -3,11 +3,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(PropuestaTesis)
-admin.site.register(Causas)
-admin.site.register(Consecuencias)
-admin.site.register(Aportes)
-admin.site.register(Variables)
-admin.site.register(ObjetivosEsp)
-admin.site.register(HipotesisEsp)
 admin.site.register(Tesis)
 admin.site.register(Observaciones)
